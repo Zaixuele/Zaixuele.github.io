3482e6960222a6a24871ee695bff6b515e47e942
Zaixuele <836967865@qq.com>
2024-09-19 14:15:58 +0800

    Site updated: 2024-09-19 14:15:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/26/hello-world/","2024/09/01/周报/周报01-为什么写周报/","2024/08/27/博客相关/使用Hexo-Github搭建个人博客/"];function toRandomPost(){
+var posts=["2024/09/01/周报/周报01-为什么写周报/","2024/08/27/博客相关/使用Hexo-Github搭建个人博客/","2024/08/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
